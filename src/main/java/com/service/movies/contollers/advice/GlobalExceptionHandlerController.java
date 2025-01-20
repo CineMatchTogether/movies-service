b70788d8057edcc0972339682928870f08aba693
ThreeAi <alexandrovaa2003@gmail.com>
2025-01-20 14:01:11 +0500

    add deploy

--- src/main/java/com/service/movies/contollers/advice/GlobalExceptionHandlerController.java
+++ src/main/java/com/service/movies/contollers/advice/GlobalExceptionHandlerController.java
@@ -2,6 +2,7 @@ package com.service.movies.contollers.advice;
 
 import com.service.movies.models.dto.ExceptionResponse;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
@@ -11,13 +12,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 @ControllerAdvice
+@Hidden
 public class GlobalExceptionHandlerController {
 
     Logger logger = LogManager.getLogger(GlobalExceptionHandlerController.class);
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex) {
-        logger.error(ex.getLocalizedMessage());
+        logger.error("Exception handler", ex.fillInStackTrace());
         return new ResponseEntity<>(new ExceptionResponse(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
